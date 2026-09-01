ff703cca432746d6b275d0ab235419faa0e715dc
shneyder0212 <101946886+shneyder0212@users.noreply.github.com>
2026-09-01 23:26:40 +0200

    Delete app directory

--- app/static/sw.js
+++
@@ -1,3 +0,0 @@
-const C='ojo99-v6';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(x=>x.addAll(['/','/manifest.webmanifest']))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
