884d228921b6a9c7ff081fe4c583edc483fc2319
shneyder0212 <101946886+shneyder0212@users.noreply.github.com>
2026-09-03 14:28:29 +0200

    fix: evitar que Analizar quede bloqueado al despertar Render

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,4 +1,4 @@
-const C='ojo99-v9-c16-operation-3of3';
+const C='ojo99-v9-c16-fast-analysis';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(x=>x.addAll(['/','/manifest.webmanifest']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
